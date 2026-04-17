065c80748c0088acc4f5925b7b62fc671f231ef6
carl-josiah <carl_josiah@yahoo.com>
2026-04-17 20:25:56 +0400

    feat: add classes in user and add requirements.txt and tests folder

--- src/models/user.py
+++ src/models/user.py
@@ -1,2 +1,22 @@
 class User:
-    """base class: inheritance and abstraction"""
+    """base class btw: inheritance and abstraction"""
+
+    def __init__(self, userID: str, name: str, email: str, password: str):
+        self.userID = userID
+        self.name = name
+        self.email = email
+        self.password = password
+
+
+class Customer(User):
+    def __init__(
+        self, customerID: str, name: str, email: str, password: str, phoneNumber: str
+    ):
+        super().__init__(customerID, name, email, password)
+        self.phoneNumber = phoneNumber
+
+
+class Staff(User):
+    def __init__(self, staffID: str, name: str, email: str, password: str, role: str):
+        super().__init__(staffID, name, email, password)
+        self.role = role
---
+++ tests/README.md
@@ -0,0 +1,3 @@
+# tests folder
+
+This folder is to check for errors, faults, crashes and such; quality control as they say.
