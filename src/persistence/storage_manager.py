9de4a52e88ac19e57aa71996174a5883c7747030
carl-josiah <carl_josiah@yahoo.com>
2026-04-18 22:36:58 +0400

    feat: implement StorageManager as a repository facade

---
+++ src/persistence/storage_manager.py
@@ -0,0 +1,42 @@
+import json
+import os
+
+
+class StorageManager:
+    def __init__(self):
+        self.base_path = os.path.join(os.path.dirname(__file__), "../../data")
+        self.files = {
+            "users": "users.json",
+            "tables": "tables.json",
+            "reservations": "reservations.json",
+            "config": "restaurant_config.json",
+        }
+
+    def _load_file(self, file_key):
+        """private method that helps read JSON data"""
+        path = os.path.join(self.base_path, self.files[file_key])
+        try:
+            with open(path, "r") as f:
+                return json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
+
+    def _save_file(self, file_key, data):
+        """private method that helps write JSON data"""
+        path = os.path.join(self.base_path, self.files[file_key])
+        with open(path, "w") as f:
+            json.dump(data, f, indent=4)
+
+    def load_tables(self):
+        return self._load_file("tables")
+
+    def load_reservations(self):
+        return self._load_file("reservations")
+
+    def save_reservations(self, reservation_data):
+        all_reservations = self.load_reservations()
+        all_reservations.append(reservation_data)
+        self._save_file("reservations", all_reservations)
+
+    def load_config(self):
+        return self._load_file("config")
